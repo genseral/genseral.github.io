b7dfbd0c4d9e35562a7134174196f8f2d026976a
Alex Genser <87755945+genseral@users.noreply.github.com>
2022-12-05 10:20:58 +0100

    adding background-img change for all pages

---
+++ assets/js/changePic.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    var img_names = ['../../images/street1.jpeg', '../../images/street2.jpg', 
+    '../../images/street3.jpeg', '../../images/street4.jpeg', '../../images/mountain1.jpeg',
+    '../../images/mountain2.jpeg', '../../images/landscape1.jpeg', '../../images/landscape2.jpeg',
+    '../../images/landscape3.jpeg'];
+
+    var image = img_names[Math.floor(Math.random()*img_names.length)];
+    var path = window.location.pathname
+    var page_name = path.split('/').pop();
+
+    if (page_name == 'index.html') {
+        document.getElementById('banner').style.backgroundImage =  "linear-gradient(to top, rgba(46, 49, 65, 0.8), rgba(46, 49, 65, 0.8)), url("+image+")";
+        document.getElementById('banner').style.backgroundRepeat = "no-repeat";
+        document.getElementById('banner').style.backgroundSize = "cover";   
+    }
+    else {
+        document.getElementsByTagName('header')[1].style.backgroundImage =  "linear-gradient(to top, rgba(46, 49, 65, 0.8), rgba(46, 49, 65, 0.8)), url("+image+")";
+        document.getElementsByTagName('header')[1].style.backgroundRepeat = "no-repeat";
+        document.getElementsByTagName('header')[1].style.backgroundSize = "cover";   
+ 
+    } 
+}
